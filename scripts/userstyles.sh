07525fe02a72e97752a769776d6615cd26d0a8e6
Tnixc <tnixxc@gmail.com>
2025-01-02 10:59:25 +0800

    update userstyles

--- scripts/userstyles.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget -O import.json https://github.com/catppuccin/userstyles/releases/download/all-userstyles-export/import.json && \
-replacer ./import.json && mv import.json ../userstyles/userstyles-import.json
